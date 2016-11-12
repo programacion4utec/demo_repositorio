978106dcfbea20199182436596cda3374c01cefa
programacion4utec <1741602007@mail.utec.edu.sv>
2016-11-12 07:01:45 -0600

    agregacion de variables
    se agregaron las variables Y y Z

--- claseA.java
+++ claseA.java
@@ -1,8 +1,12 @@
 public class claseA{
 
-    private int x;
+            private int x;
+            private int y;
+            private int z;
     
     public claseA(){
-    x=0;
+            x=0;
+            y=0;
+            z=0;
     }
  }
